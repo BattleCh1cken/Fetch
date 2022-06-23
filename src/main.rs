b12cad5e218174f07d90e685ffce161ad51e2e84
BattleCh1cken <BattleCh1cken@Larkov.de>
2022-06-23 12:51:41 -0400

    added a nice table

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
-use systemstat::{Duration, Platform, System};
-fn main() {
-    println!("Username: {}", whoami::username());
-    println!("System: {}", whoami::distro());
+mod colors;
+use std::array;
 
-    let sys = System::new();
+use colors::*;
+use comfy_table::Table;
+use comfy_table::TableComponent::*;
+
+fn main() {
+    println!("{RED}{}@{BLUE}{}", whoami::username(), whoami::hostname());
+    let mut table = Table::new();
+    table
+        .add_row(vec!["Insert ascii art here", "hostname etc"])
+        .add_row(vec!["", "system info here"]);
+    let components = [
+        LeftBorder,
+        RightBorder,
+        TopBorder,
+        BottomBorder,
+        LeftHeaderIntersection,
+        HeaderLines,
+        MiddleHeaderIntersections,
+        RightHeaderIntersection,
+        VerticalLines,
+        HorizontalLines,
+        MiddleIntersections,
+        LeftBorderIntersections,
+        RightBorderIntersections,
+        TopBorderIntersections,
+        BottomBorderIntersections,
+        TopLeftCorner,
+        TopRightCorner,
+        BottomLeftCorner,
+        BottomRightCorner,
+    ];
+
+    for component in components {
+        table.set_style(component, ' ');
+    }
+
+    println!("{table}")
 }
